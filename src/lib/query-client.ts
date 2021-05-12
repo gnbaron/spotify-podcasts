0e939e0265cc264a027edbf7e47c2ccd265dbee4
Guilherme Baron <floriani.guilherme@gmail.com>
2021-05-12 10:59:40 -0300

    refetch current query after refreshing the access token

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -7,13 +7,13 @@ export const queryClient = new QueryClient()
  * Make a request for Spotify WEB API.
  * @param url resource url
  * @param options fetch options
- * @returns promise for the response
+ * @returns response promise
  * @throws Error when access token is invalid
  */
-export const fetchSpotifyAPI = async (
+export const fetchSpotifyAPI = async <T>(
   url: RequestInfo,
   options?: RequestInit
-) => {
+): Promise<T> => {
   const tokens = TokenStorage.read()
   if (!tokens) {
     throw new Error('Invalid access token')
@@ -25,6 +25,7 @@ export const fetchSpotifyAPI = async (
   const response = await fetch(url, { headers, ...options })
   if (response.status === 401) {
     await queryClient.refetchQueries('tokens')
+    return fetchSpotifyAPI(url, options)
   }
   if (!response.ok) {
     throw new Error('Unexpected error')
