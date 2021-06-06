359e371cfae746115bc057ed93f66571985f3a7e
Guilherme Baron <floriani.guilherme@gmail.com>
2021-06-06 16:54:35 -0300

    add google analytics support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document'
+import { Analytics } from 'components/Analytics'
 import { GithubLink } from 'components/GithubLink'
 
 class MyDocument extends Document {
@@ -21,6 +22,7 @@ class MyDocument extends Document {
           <GithubLink />
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
