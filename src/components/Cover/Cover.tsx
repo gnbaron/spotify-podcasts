61163aaecabc068ac4580d745e207d290fd34f46
Guilherme Baron <floriani.guilherme@gmail.com>
2021-06-18 16:02:15 -0300

    lazy load cover images

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import classNames from 'classnames'
+import { useIntersectionObserver } from 'hooks/use-intersection-observer'
 
 import styles from './Cover.module.css'
 
@@ -8,10 +10,22 @@ type Props = {
   size: 's' | 'm' | 'l'
 }
 
-export const Cover = ({ className, image, size }: Props) => (
-  <img
-    className={classNames(className, styles.cover, styles[size])}
-    src={image.url}
-    role="presentation"
-  />
-)
+export const Cover = ({ className, image, size }: Props) => {
+  const [loaded, setLoaded] = useState(false)
+  const [imageRef, setImageRef] = useState<HTMLImageElement | null>(null)
+
+  const observer = useIntersectionObserver(imageRef)
+  const src = observer?.isIntersecting || loaded ? image.url : undefined
+
+  return (
+    <div className={classNames(styles.wrapper, loaded && styles.loaded)}>
+      <img
+        className={classNames(className, styles.cover, styles[size])}
+        onLoad={() => setLoaded(true)}
+        src={src}
+        role="presentation"
+        ref={setImageRef}
+      />
+    </div>
+  )
+}
