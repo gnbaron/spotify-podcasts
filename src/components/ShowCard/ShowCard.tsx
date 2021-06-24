809f094c6bb77f78ceeeffc53164625fad63ca10
Guilherme Baron <floriani.guilherme@gmail.com>
2021-06-24 20:54:26 -0300

    make show and episode card responsive

--- src/components/ShowCard/ShowCard.tsx
+++ src/components/ShowCard/ShowCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { useIsWideScreen } from 'utils/use-media-query'
 import { Cover } from 'components/Cover'
 
 import styles from './ShowCard.module.css'
@@ -7,18 +8,23 @@ type Props = {
   show: SpotifyApi.ShowObjectSimplified
 }
 
-export const ShowCard = ({ show }: Props) => (
-  <div className={styles.card}>
-    <Link to={`/shows/${show.id}`}>
-      <header>
-        <Cover
-          className={styles.cover}
-          image={show.images[1] || show.images[0]}
-          size="m"
-        />
-        <p className={styles.name}>{show.name}</p>
-      </header>
-    </Link>
-    <small className={styles.publisher}>{show.publisher}</small>
-  </div>
-)
+export const ShowCard = ({ show }: Props) => {
+  const isWide = useIsWideScreen()
+  return (
+    <div className={styles.card}>
+      <Link to={`/shows/${show.id}`}>
+        <div className={styles.body}>
+          <Cover
+            className={styles.cover}
+            image={show.images[1] || show.images[0]}
+            size={isWide ? 'm' : 'xs'}
+          />
+          <div>
+            <p className={styles.name}>{show.name}</p>
+            <p className={styles.publisher}>{show.publisher}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
