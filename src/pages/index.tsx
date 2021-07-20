917a7f599319b48c8a7b79c364ffc36383c13852
Guilherme Baron <floriani.guilherme@gmail.com>
2021-07-20 19:14:06 -0300

    feat(index page): make page static instead of SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,27 @@
-import { NextPageContext } from 'next'
-import { Tokens } from 'types/common'
+import { useEffect, useState } from 'react'
 import { App, Providers } from 'app'
+import { Tokens } from 'types/common'
 
-type Props = {
-  tokens: Tokens | null
-}
+export default function IndexPage() {
+  const [tokens, setTokens] = useState<Tokens | null | undefined>(undefined)
 
-export default function IndexPage({ tokens }: Props) {
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search)
+    const accessToken = query.get('accessToken')
+    const refreshToken = query.get('refreshToken')
+    if (accessToken && refreshToken) {
+      setTokens({ accessToken, refreshToken })
+    } else {
+      setTokens(null)
+    }
+  }, [])
+
+  // does not redirect to /login before query string is parsed
+  if (tokens === undefined) return null
+
   return (
     <Providers tokens={tokens}>
       <App />
     </Providers>
   )
-}
-
-export function getServerSideProps({ query }: NextPageContext) {
-  const { accessToken, refreshToken } = query
-  let tokens = null
-  if (accessToken && refreshToken) {
-    tokens = { accessToken, refreshToken }
-  }
-  return {
-    props: { tokens },
-  }
 }
